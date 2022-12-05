b93179b4e8ee186e9d51e5ec91f97a2767088999
user <user@DESKTOP-60EJFL9>
2022-12-05 14:11:56 +0900

    change Apple class

--- src/fruit/Apple.java
+++ src/fruit/Apple.java
@@ -3,5 +3,17 @@ package fruit;
 public class Apple {
 	int price;
 	int grade;
+	boolean clean;
+	
+	public void ate() {
+		price -= 1000;
+	}
+	
+	public void clean() {
+		clean = true;
+	}
+	
+
+	
 	
 }
